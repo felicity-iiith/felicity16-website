5284359e8922deb3a2684e2953b905deb91cd3a2
Nisarg Jhaveri <nisargjhaveri@gmail.com>
2015-12-19 15:10:07 +0530

    Add flash data support in session library

---
+++ src/app/libraries/session_lib.php
@@ -0,0 +1,38 @@
+<?php
+
+class session_lib extends Library {
+
+    public function __construct() {
+        if (session_status() == PHP_SESSION_NONE) {
+            session_start();
+        }
+    }
+
+    /**
+     * Set flash data
+     * @param string $name
+     * @param mixed  $value
+     */
+    public function flash_set($name, $value) {
+        if (!isset($_SESSION["_flashdata"])) {
+            $_SESSION["_flashdata"] = [];
+        }
+        $_SESSION["_flashdata"][$name] = $value;
+    }
+
+    /**
+     * Get and unset flash data
+     * @param  string     $name
+     * @return mixed|null       Value of the set flashed data
+     */
+    public function flash_get($name) {
+        if (!isset($_SESSION["_flashdata"])
+            || !isset($_SESSION["_flashdata"][$name])
+        ) {
+            return null;
+        }
+        $value = $_SESSION["_flashdata"][$name];
+        unset($_SESSION["_flashdata"][$name]);
+        return $value;
+    }
+}
