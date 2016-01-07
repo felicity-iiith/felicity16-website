df59d3f99f6ed238735e174e3ecdd13534e1202f
Harshit Harchani <hharchani@gmail.com>
2016-01-08 02:45:25 +0530

    Add coming soon template

---
+++ src/app/views/templates/coming_soon/index.php
@@ -0,0 +1,9 @@
+<?php $this->load_fragment('skeleton_template/header'); ?>
+<?php if (!$is_ajax): ?>
+<article class="page contact open">
+<?php endif; ?>
+    <h1 class="text-center">This page is coming soon</h1>
+<?php if (!$is_ajax): ?>
+</article>
+<?php endif; ?>
+<?php $this->load_fragment('skeleton_template/footer'); ?>
