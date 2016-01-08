24f47abbce9b485a4695a2cb27dd34e5e082399b
Harshit Harchani <hharchani@gmail.com>
2016-01-08 18:51:22 +0530

    Remove full page class from coming soon template

--- src/app/views/templates/coming_soon/index.php
+++ src/app/views/templates/coming_soon/index.php
@@ -1,6 +1,6 @@
 <?php $this->load_fragment('skeleton_template/header'); ?>
 <?php if (!$is_ajax): ?>
-<article class="page open full coming_soon">
+<article class="page open coming_soon">
 <?php endif; ?>
     <h1 class="text-center">This page is coming soon</h1>
 <?php if (!$is_ajax): ?>
