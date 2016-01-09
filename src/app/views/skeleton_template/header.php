9350e92c8cd68204fe3fb5249d6cef7ceca942bf
Harshit Harchani <hharchani@gmail.com>
2016-01-09 22:11:43 +0530

    Add og image jugaadable, add meta

--- src/app/views/skeleton_template/header.php
+++ src/app/views/skeleton_template/header.php
@@ -5,14 +5,18 @@ if (empty($is_ajax)):
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+    <meta name="keywords" content="felicity, felicty16, college, fest, threads, pulsation, iiit, iiith, international, institute, information, technology, hyderabad">
+    <meta name="description" content="Felicity is the annual technical and cultural fest of IIIT-H. Includes technical, cultural and literary events, Major nights, talks, workshops and performances. We, at IIIT-H, believe in giving back to the society and use Felicity as a medium to serve this motive and pickup various social initiatives.">
+    <meta property="og:description" content="Felicity is the annual technical and cultural fest of IIIT-H. Includes technical, cultural and literary events, Major nights, talks, workshops and performances. We, at IIIT-H, believe in giving back to the society and use Felicity as a medium to serve this motive and pickup various social initiatives.">
+    <meta property="og:title" content="<?= implode(' · ', array_reverse(array_map(function($str){ return ucfirst(str_replace('-', ' ', $str)); }, explode('__', $page_slug)))) ?> Felicity · IIIT Hyderabad">
+    <meta property="og:image" content="<?= base_url() . (isset($og_image) ? $og_image : 'files/16/logos/felicity16-logo-large.png') ?>" />
     <title>Felicity · IIIT Hyderabad</title>
     <link rel="icon" href="<?= base_url() ?>favicon.ico">
     <link rel="stylesheet" href="<?= base_url() ?>static/styles/vendor/normalize.min.css">
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/pure/0.6.0/pure-min.css">
     <link rel="stylesheet" href="<?= base_url() ?>static/styles/vendor/felicons.css">
     <link rel="stylesheet" href="<?= base_url() ?>static/styles/core.css">
-    <link rel="stylesheet" href="<?= base_url() ?>static/styles/schedule.css">
-    <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Nothing+You+Could+Do|Flamenco|Noto+Sans">
+    <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Flamenco|Noto+Sans">
     <script type="text/javascript">
         var baseUrl = '<?= base_url() ?>';
     </script>
