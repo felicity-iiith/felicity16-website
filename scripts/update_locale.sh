ef3974e7e35654f84076d13a92892b8c6bec536c
Nisarg Jhaveri <nisargjhaveri@gmail.com>
2016-01-29 18:05:52 +0530

    Add locale files

---
+++ scripts/update_locale.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+locales=("hi_IN" "te_IN" "gu_IN")
+
+echo "Getting locale_dump from https://felicity.iiit.ac.in"
+status=$(curl -# --write-out "%{http_code}" "https://felicity.iiit.ac.in/jugaad/locale_dump/" -o "src/locale/locale_dump.php")
+if [ $status -ne 200 ]
+then
+    echo "Could not get file from server"
+    exit 1
+fi
+
+dos2unix src/locale/locale_dump.php
+
+echo -n "Generating .pot file"
+find src -iname "*.php" | xargs xgettext -L PHP -k__ -k_ -kgettext --from-code utf-8 -o src/locale/messages.pot
+echo " ..... done."
+for locale in ${locales[@]}
+do
+    echo "Merging $locale"
+    msgmerge -U src/locale/${locale}/LC_MESSAGES/messages.po src/locale/messages.pot
+done
