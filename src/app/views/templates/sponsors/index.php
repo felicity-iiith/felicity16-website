b562298ba620f1f2748155b7aec59f5786f2ce10
Vivek Ghaisas <v.a.ghaisas@gmail.com>
2016-02-11 14:21:05 +0530

    Add some 2016 sponsors

--- src/app/views/templates/sponsors/index.php
+++ src/app/views/templates/sponsors/index.php
@@ -3,6 +3,10 @@
 <article class="page open sponsors">
 <?php endif; ?>
 <?php
+$sponsors2016 = [
+    ['name' => 'Progress Software', 'image' => 'progress.jpg'],
+];
+
 $sponsors2015 = [
     ['name' => 'Ebay', 'image' => 'ebay.png'],
     ['name' => 'Freecharge', 'image' => 'freecharge.png'],
@@ -39,6 +43,55 @@ $sponsors2014 = [
 ];
 ?>
     <div class="container">
+        <h2><?= __('Felicity 2016 Sponsors') ?></h2>
+        <div class="row some-top-margin">
+            <div class="col6 offset3">
+                <img src="<?= base_url() ?>static/images/sponsors/qualcomm.jpg" alt="Qualcomm logo"/>
+                <p>Title Sponsor</p>
+            </div>
+        </div>
+        <div class="row some-top-margin">
+            <div class="col4 container">
+                <img src="<?= base_url() ?>static/images/sponsors/microsoft.png" alt="Microsoft Logo"/>
+                <p><a class="underlined" href="<?= base_url() ?>threads/kings-of-ml/">Kings of ML</a> and <a class="underlined" href="<?= base_url() ?>threads/botomata/">Botomata</a> sponsor</p>
+            </div>
+            <div class="col4 container">
+                <img src="<?= base_url() ?>static/images/sponsors/indeed.png" alt="Indeed Logo"/>
+                <p>Nights Sponsor</p>
+            </div>
+            <div class="col4 container">
+                <img src="<?= base_url() ?>static/images/sponsors/sbh.png" alt="SBH Logo"/>
+                <p>Official Banking Partner</p>
+            </div>
+        </div>
+        <div class="row some-top-margin">
+            <div class="col3 container">
+                <img src="<?= base_url() ?>static/images/sponsors/cornetto.png" alt="Cornetto Logo"/>
+                <p>Official Snacking Partner</p>
+            </div>
+            <div class="col3 container">
+                <img src="<?= base_url() ?>static/images/sponsors/9xm.png" alt="9XM Logo"/>
+                <p>Powered By Partner</p>
+            </div>
+            <div class="col3 container">
+                <img style="background-color: #0F69D6" src="<?= base_url() ?>static/images/sponsors/9xo.png" alt="9XO Logo"/>
+                <p>Powered By Partner</p>
+            </div>
+            <div class="col3 container">
+                <img src="<?= base_url() ?>static/images/sponsors/ccd.png" alt="CCD Logo"/>
+                <p>Café Partners</p>
+            </div>
+        </div>
+        <?php foreach (array_chunk($sponsors2016, 4) as $sponsors): ?>
+            <div class="row some-top-margin">
+                <?php foreach ($sponsors as $sponsor): ?>
+                    <div class="col3 container">
+                        <img src="<?= base_url() .'static/images/sponsors/'. $sponsor['image'] ?>" alt="<?= $sponsor['name'] ?> Logo" />
+                        <p><?= $sponsor['name'] ?></p>
+                    </div>
+                <?php endforeach; ?>
+            </div>
+        <?php endforeach; ?>
         <h2><?= __('Past sponsors') ?></h2>
         <h3><?= __('Year 2015') ?></h3>
         <div class="row some-top-margin">
