28872d9ab1b1bc5a35f15d636a5370bbda3fa1d1
Harshit Harchani <hharchani@gmail.com>
2016-02-11 06:47:12 +0530

    Make some fields optional in catogory template

--- src/app/views/templates/category/index.php
+++ src/app/views/templates/category/index.php
@@ -25,7 +25,9 @@ usort($events_to_display, function ($e1, $e2) {
 ?>
 <div class="container text-center">
     <h1><?= __($name) ?></h1>
-    <p class="lead"><?= __($tagline) ?></p>
+    <?php if (!empty($tagline)): ?>
+        <p class="lead"><?= __($tagline) ?></p>
+    <?php endif; ?>
     <div class="row">
         <div class="col6">
             <p class="lead text-justify some-top-margin"><?= nl2br(__($long_description)) ?></p>
@@ -37,7 +39,9 @@ usort($events_to_display, function ($e1, $e2) {
                     <?php foreach ($events_to_display as $event): ?>
                         <a class="event btn" href="<?= locale_base_url() . substr($event['path'], 1) ?>">
                             <p class="lead"><?= __($event['data']['name']) ?></p>
-                            <p><small><?= __($event['data']['tagline']) ?></small></p>
+                            <?php if (!empty($event['data']['tagline'])): ?>
+                                <p><small><?= __($event['data']['tagline']) ?></small></p>
+                            <?php endif; ?>
                             <p><small><?= ($d = $event['data']['start_time']) ? strftime('%B %e', date_timestamp_get(date_create($d))) : __('To be announced') ?></small></p>
                         </a>
                     <?php endforeach; ?>
