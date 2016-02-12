45dd10a6cdfd0cd2f2de14a5be42c8a2bac74004
Harshit Harchani <hharchani@gmail.com>
2016-02-13 03:37:13 +0530

    Add codecraft scoreboard

--- src/app/controllers/static_page.php
+++ src/app/controllers/static_page.php
@@ -17,4 +17,8 @@ class static_page extends Controller {
     public function register() {
         $this->http_lib->redirect( locale_base_url() . 'auth/login/' );
     }
+
+    public function codecraft() {
+        $this->load_view('contest/codecraft_scores');
+    }
 }
