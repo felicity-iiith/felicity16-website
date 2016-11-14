9dd1ed267f0cd44a14951afba06c16462544e8b3
Anshul Singhal <sganshuliiith@gmail.com>
2016-11-14 16:22:35 +0530

    [felicity17] Removes plax and adds parallax

--- src/app/views/skeleton_template/header.php
+++ src/app/views/skeleton_template/header.php
@@ -32,27 +32,10 @@ if (empty($is_ajax)):
     <title><?= isset($title) ? $title . ' · ' : '' ?><?= __('Felicity') ?> · <?= __('IIIT-Hyderabad') ?></title>
 
     <link rel="icon" href="<?= base_url() ?>favicon.ico">
-    <script type="text/javascript" src="<?= base_url() ?>static/scripts/vendor/jquery.min.js"></script>
-    <script type="text/javascript" src="<?= base_url() ?>static/scripts/vendor/plax.js"></script>
     <link rel="stylesheet" type="text/css" href="<?= base_url() ?>static/styles/new.css">
-    <link rel="stylesheet" type="text/css" href="<?= base_url() ?>static/styles/panel.css">
     <script type="text/javascript">
         var baseUrl = '<?= base_url() ?>';
         var localeBaseUrl = '<?= locale_base_url() ?>';
-    </script>
-    <script type="text/javascript">
-      $(document).ready(function () {
-        $('.mountain').plaxify({"xRange":5,"yRange":5})
-        $('.land2').plaxify({"xRange":20,"yRange":20})
-        $('.land3').plaxify({"xRange":25,"yRange":25})
-        $('.land').plaxify({"xRange":40,"yRange":40})
-        $('.house').plaxify({"xRange":30,"yRange":30})
-        $('.title').plaxify({"xRange":100,"yRange":100})
-        // $('.wave').plaxify({"xRange":40,"yRange":40,"invert":false})
-        $('.boat').plaxify({"xRange":40,"yRange":40,"invert":false})
-
-        $.plax.enable()
-      })
     </script>
 </head>
 <?php
@@ -61,91 +44,186 @@ if (empty($is_ajax)):
     }
 ?>
 <body>
-    <img src="<?= base_url() ?>static/images/home/background.png" class="mountain"></div>
-    <div class="land2"></div>
-    <div class="land3"></div>
-    <div class="house"></div>
-    <div class="land"></div>
-    <div class="wave plain depth-60"></div>
-    <div class="wave paint depth-50"></div>
-    <div class="wave paint depth-40"></div>
-    <div class="wave plain depth-30"></div>
-    <div class="wave paint depth-20"></div>
-    <div class="wave plain depth-10"></div>
-    <!--div class="wave plain depth-40"></div>
-    <div class="wave paint depth-50"></div>
-    <h1 class="title"><b>felicity<em>17</em></b></h1>
-    <div class="wave plain depth-0"></div>
-    <div class="boat"></div>
-    <div class="wave paint depth-60"></div>
-    <div class="wave plain depth-80"></div>
-    <div class="wave paint depth-100"></div>
-    <div onclick="showDetails('About')" class="cloud cloud-1">About</div>
-    <div onclick="showDetails('Gallery')" class="cloud cloud-2">Gallery</div>
-    <div onclick="showDetails('Schedule')" class="cloud cloud-3">Schedule</div>
-    <div onclick="showDetails('Sponsors')" class="cloud cloud-4">Sponsors</div>
-    <div onclick="showDetails('Team')" class="cloud cloud-5">Team</div> -->
-    <section id="about" class="about hide">
-        <div class="cell">
-            <div class="cables center accelerate">
-                <div class="linkholder">
-                    <ul class="links">
-                        <li><a>Button 1</a></li>
-                        <li><a>Button 2</a></li>
-                        <li><a>Button 3</a></li>
-                        <li><a>Button 4</a></li>
-                    </ul>
-                </div>
-                <div class="panel accelerate">
-                    <header>
-                        <h1 onclick="alert(1)">felicity<em>17</em></h1>
-                    </header>
-                    <p>Insert random content here</p>
-                    <p>Insert random content here</p>
-                    <p>Insert random content here</p>
-                </div>
-            </div>
+  <div id="fb-root"></div>
+
+    <div id="container" class="wrapper">
+      <ul id="scene" class="scene unselectable"
+        data-friction-x="0.1"
+        data-friction-y="0.1"
+        data-scalar-x="25"
+        data-scalar-y="15">
+      <li class="layer" data-depth="0.00"><div class="background"></div></li>
+      <li class="layer" data-depth="0.08"><div class="mountain"></div></li>
+      <!--li class="layer" data-depth="0.15">
+        <ul class="rope depth-10">
+        <li><img src="assets/images/rope.png" alt="Rope"></li>
+        <li class="hanger position-2">
+          <div class="board cloud-2 swing-1"></div>
+        </li>
+        <li class="hanger position-4">
+          <div class="board cloud-1 swing-3"></div>
+        </li>
+        <li class="hanger position-8">
+          <div class="board birds swing-5"></div>
+        </li>
+        </ul>
+      </li>
+      <li class="layer" data-depth="0.20"><h1 class="title"><b>felicity<em>17</em></b></h1></li>
+      <li class="layer" data-depth="0.30">
+        <ul class="rope depth-30">
+        <li><img src="assets/images/rope.png" alt="Rope"></li>
+        <li class="hanger position-1">
+          <div class="board cloud-1 swing-3"></div>
+        </li>
+        <li class="hanger position-5">
+          <div class="board cloud-4 swing-1"></div>
+        </li>
+        </ul>
+      </li-->
+      <li class="layer" data-depth="0.20"><div class="land2"></div></li>
+      <li class="layer" data-depth="0.21"><div class="land3"></div></li>
+      <li class="layer" data-depth="0.23"><div class="house"></div></li>
+      <li class="layer" data-depth="0.27"><div class="land"></div></li>
+      <li class="layer" data-depth="0.40"><div class="wave plain depth-60"></div></li>
+      <li class="layer" data-depth="0.50"><div class="wave paint depth-50"></div></li>
+      <li class="layer" data-depth="0.28"><h1 class="title"><b>felicity<em>17</em></b></h1></li>
+      <!--li class="layer" data-depth="0.60"><div class="lighthouse depth-60"></div></li -->
+      <!--li class="layer" data-depth="0.60">
+        <ul class="rope depth-60">
+        <li><img src="assets/images/rope.png" alt="Rope"></li>
+        <li class="hanger position-3">
+          <div class="board birds swing-5"></div>
+        </li>
+        <li class="hanger position-6">
+          <div class="board cloud-2 swing-2"></div>
+        </li>
+        <li class="hanger position-8">
+          <div class="board cloud-3 swing-4"></div>
+        </li>
+        </ul>
+      </li-->
+      <li class="layer" data-depth="0.60"><div class="wave paint depth-40"></div></li>
+      <!--li class="layer" data-depth="0.65"><div class="boat"></div></li-->
+      <li class="layer" data-depth="0.70"><div class="wave plain depth-30"></div></li>
+      <li class="layer" data-depth="0.80"><div class="wave paint depth-20"></div></li>
+      <li class="layer" data-depth="0.90"><div class="wave plain depth-10"></div></li>
+      <li class="layer" data-depth="0.15"><div onclick="showDetails('About')" class="cloud cloud-1">About</div></li>
+      <li class="layer" data-depth="0.09"><div onclick="showDetails('Gallery')" class="cloud cloud-2">Gallery</div></li>
+      <li class="layer" data-depth="0.15"><div onclick="showDetails('Schedule')" class="cloud cloud-3">Schedule</div></li>
+      <li class="layer" data-depth="0.09"><div onclick="showDetails('Sponsors')" class="cloud cloud-4">Sponsors</div></li>
+      <li class="layer" data-depth="0.15"><div onclick="showDetails('Team')" class="cloud cloud-5">Team</div></li>
+      </ul>
+      <section id="about" class="wrapper about hide accelerate">
+      <div class="cell accelerate">
+        <div class="cables center accelerate">
+        <div class="panel accelerate">
+          <ul class="links">
+          <li><a>Login</a></li>
+          <li><a target="_blank" >Register</a></li>
+          </ul>
+          <header>
+          <h1>felicity<em>17</em></h1>
+          </header>
+          <p>Insert random content here</p>
         </div>
-    </section>
-    <button id="toggle" class="toggle i">
-        <div class="cross">
-            <div class="x"></div>
-            <div class="y"></div>
         </div>
-    </button>
-    <?php if (isset($is_authenticated)): ?>
-    <div class="auth-quick-links">
-        <?php if ($is_authenticated): ?>
-            <?php if (!empty($user_nick)): ?>
-                <div class="nick"><?= sprintf(__('Hello, %s'), $user_nick) ?> <a href="<?= locale_base_url() . "auth/logout/" ?>"><?= __('Logout') ?></a></div>
-            <?php else: ?>
-                <div><a href="<?= locale_base_url() . "auth/logout/" ?>" class="pure-button btn"><?= __('Logout') ?></a></div>
-            <?php endif; ?>
-        <?php else: ?>
-            <div><a onclick="window.location.href = '<?= locale_base_url() . "auth/login/" ?>';" class="pure-button btn"><?= __('Login / Register') ?></a></div>
-        <?php endif; ?>
-    </div>
-    <?php endif; ?>
-    <script>
-        var $html = $('html'),
-            $toggle = $('#toggle'),
-            $about = $('#about'),
-            $scene = $('#scene');
+      </div>
+      </section>
+      <button id="toggle" class="toggle i">
+      <div class="cross">
+        <div class="x"></div>
+        <div class="y"></div>
+      </div>
+      </button>
+      <!-- Scripts -->
+      <script src="<?= base_url() ?>static/scripts/libraries.min.js"></script>
+      <script src="<?= base_url() ?>static/scripts/jquery.parallax.js"></script>
+      <script>
 
-        function showDetails() {
-            $about.removeClass('hide');
-            $toggle.removeClass('i');
+      // jQuery Selections
+      var $html = $('html'),
+      $container = $('#container'),
+      $prompt = $('#prompt'),
+      $toggle = $('#toggle'),
+      $about = $('#about'),
+      $scene = $('#scene');
+
+      // Hide browser menu.
+      (function() {
+        setTimeout(function(){window.scrollTo(0,0);},0);
+      })();
+
+      // Setup FastClick.
+      FastClick.attach(document.body);
+
+      // Add touch functionality.
+      if (Hammer.HAS_TOUCHEVENTS) {
+        $container.hammer({drag_lock_to_axis: true});
+        _.tap($html, 'a,button,[data-tap]');
+      }
+
+      // Add touch or mouse class to html element.
+      $html.addClass(Hammer.HAS_TOUCHEVENTS ? 'touch' : 'mouse');
+
+      // Resize handler.
+      (resize = function() {
+        $scene[0].style.width = window.innerWidth + 'px';
+        $scene[0].style.height = window.innerHeight + 'px';
+        if (!$prompt.hasClass('hide')) {
+          if (window.innerWidth < 600) {
+            $toggle.addClass('hide');
+          } else {
+            $toggle.removeClass('hide');
+          }
         }
+      })();
 
-        function hideDetails() {
-            $about.addClass('hide');
-            $toggle.addClass('i');
+      // Attach window listeners.
+      window.onresize = _.debounce(resize, 200);
+      window.onscroll = _.debounce(resize, 200);
+
+      function showDetails(type) {
+        console.log(type);
+        $about.removeClass('hide');
+        $about.slideDown();
+        $toggle.removeClass('i');
+      }
+
+      function hideDetails() {
+        $about.addClass('hide');
+        $about.slideUp();
+        $toggle.addClass('i');
+      }
+
+      function toggleDetails(event) {
+        $toggle.hasClass('i') ? showDetails() : hideDetails();
+      }
+        hideDetails();
+      // Listen for toggle click event.
+      $toggle.on('click', function(event) {
+        $toggle.hasClass('i') ? showDetails() : hideDetails();
+      });
+
+      // Pretty simple huh?
+      $scene.parallax();
+
+      // Check for orientation support.
+      setTimeout(function() {
+        if ($scene.data('mode') === 'cursor') {
+          $prompt.removeClass('hide');
+          if (window.innerWidth < 600) $toggle.addClass('hide');
+          $prompt.on('click', function(event) {
+            $prompt.addClass('hide');
+            if (window.innerWidth < 600) {
+              setTimeout(function() {
+                $toggle.removeClass('hide');
+              },1200);
+            }
+          });
         }
+      },1000);
+      </script>
 
-        $toggle.on('click', function(event) {
-            $toggle.hasClass('i') ? showDetails() : hideDetails();
-        });
-    </script>
   </body>
 </html>
 <?php endif;
