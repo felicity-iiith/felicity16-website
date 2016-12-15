3eedbf936d43b4345fafc93672c73231b30269be
Anshul Singhal <sganshuliiith@gmail.com>
2016-12-16 03:45:23 +0530

    [felicity17] Bug fixes for general pages(2)

--- src/app/views/templates/general/index.php
+++ src/app/views/templates/general/index.php
@@ -1,6 +1,6 @@
 <?php $this->load_fragment('skeleton_template/header', ['title' => __($title)]); ?>
 <?php if (!$is_ajax): ?>
-<article class="page open full">
+<article class="page full">
 <?php endif; ?>
     <h1 class="text-center"><?= __($title) ?></h1>
     <div class="container"><?= __($body) ?></div>
